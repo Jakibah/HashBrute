b30a4c05e03d67b34bcdef3e7cdd925592a10532
Jakibah <iakovospetrouentertainment@gmail.com>
2016-08-28 21:31:45 +0200

    Added Decrypt (Not Working Yet) Added File Reader Added list.lst

--- HashBrute/src/com/jakibah/HashBrute/Crypters.java
+++ HashBrute/src/com/jakibah/HashBrute/Crypters.java
@@ -1,17 +1,57 @@
 package com.jakibah.HashBrute;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Crypters {
 
-	public static String Encrypt(String s) throws NoSuchAlgorithmException{
+	private static File list = new File("list.lst");
+
+	public static String Encrypt(String s, boolean CloseAfterThis) {
 		MessageDigest md = null;
-		md =MessageDigest.getInstance("MD5");
-		md.update(s.getBytes(),0,s.length());
-		String encrypted = new BigInteger(1,md.digest()).toString(16);
-		Main.Stop();
-		return encrypted;
+
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+
+			e.printStackTrace();
+		}
+
+		md.update(s.getBytes(), 0, s.length());
+		String encrypted = new BigInteger(1, md.digest()).toString(16);
+		if (CloseAfterThis) {
+			Main.Stop();
+			return encrypted;
+		} else {
+			return encrypted;
+		}
+	}
+
+	public static void Decrypt(String hash) {
+		List<String> lines = null;
+
+		if (!list.exists()) {
+			try {
+				list.createNewFile();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			lines = Files.readAllLines(list.toPath(), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 }
--- HashBrute/src/com/jakibah/HashBrute/Main.java
+++ HashBrute/src/com/jakibah/HashBrute/Main.java
@@ -2,9 +2,12 @@ package com.jakibah.HashBrute;
 
 import java.security.NoSuchAlgorithmException;
 
-public class Main implements Runnable{
-	
+import javax.swing.JFrame;
+
+public class Main implements Runnable {
+
 	private static boolean running = false;
+	private JFrame MainFrame = new JFrame();
 
 	public static void main(String[] args) {
 		Thread MainThread = new Thread(new Main());
@@ -13,24 +16,20 @@ public class Main implements Runnable{
 
 	}
 
-	public static void Stop(){
+	public static void Stop() {
 		running = false;
 	}
-	
+
 	@Override
 	public void run() {
 		running = true;
-		
-		while(running){
-			
-			try {
-				System.out.println(Crypters.Encrypt("test123"));
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
-			
+
+		while (running) {
+
+			//System.out.println(Crypters.Encrypt("test123", true));
+			//Crypters.Decrypt("test123");
 		}
-		
+
 	}
 
 }
